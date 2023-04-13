b171a1c328eedc3f23d7247d389856aeeff77e0f
QiongranHuang <qh2130@nyu.edu>
2023-04-13 14:43:08 -0400

    added a new file

---
+++ script.js
@@ -0,0 +1,38 @@
+{
+  "title": "My Awesome Playlist",
+  "description": "A collection of my favorite songs",
+  "items": [
+    {
+      "title": "Bohemian Rhapsody",
+      "artist": "Queen",
+      "album": "A Night at the Opera",
+      "length": "5:54",
+      "image": "https://example.com/bohemian_rhapsody.jpg",
+      "color": "#a30000"
+    },
+    {
+      "title": "Stairway to Heaven",
+      "artist": "Led Zeppelin",
+      "album": "Led Zeppelin IV",
+      "length": "8:02",
+      "image": "https://example.com/stairway_to_heaven.jpg",
+      "color": "#0065a3"
+    },
+    {
+      "title": "Hotel California",
+      "artist": "Eagles",
+      "album": "Hotel California",
+      "length": "6:31",
+      "image": "https://example.com/hotel_california.jpg",
+      "color": "#a39000"
+    },
+    {
+      "title": "Sweet Child o' Mine",
+      "artist": "Guns N' Roses",
+      "album": "Appetite for Destruction",
+      "length": "5:56",
+      "image": "https://example.com/sweet_child_o_mine.jpg",
+      "color": "#a3008e"
+    }
+  ]
+}
